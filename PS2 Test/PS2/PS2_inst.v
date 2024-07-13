93a6c0eef5529f4c08fb0814bb525c6a2ad198d0
Malcolm Roalson <mtroalson@gmail.com>
2024-07-12 20:37:55 -0500

    PS2 test with keyboard

--- PS2 Test/PS2/PS2_inst.v
+++
@@ -1,7 +0,0 @@
-	PS2 u0 (
-		.clk_clk                      (<connected-to-clk_clk>),                      //                      clk.clk
-		.reset_reset_n                (<connected-to-reset_reset_n>),                //                    reset.reset_n
-		.ps2_0_external_interface_CLK (<connected-to-ps2_0_external_interface_CLK>), // ps2_0_external_interface.CLK
-		.ps2_0_external_interface_DAT (<connected-to-ps2_0_external_interface_DAT>)  //                         .DAT
-	);
-
--- PS2 Test/PS2_test.v
+++ PS2 Test/PS2_test.v
@@ -11,9 +11,7 @@ module PS2_test(CLOCK_50, PS2_CLK, PS2_DATA, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
 	inout  			PS2_CLK;
 	inout  			PS2_DATA;
 	input	 			TEST;
-	
-	//wire [31:0] readdata;
-	
+		
 	wire command_was_sent;
 	wire error_communication_timed_out;
 	wire [7:0] received_data;
@@ -27,34 +25,44 @@ module PS2_test(CLOCK_50, PS2_CLK, PS2_DATA, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
 		.received_data(received_data),
 		.received_data_en(received_data_en)
 	);
+		
+	reg [15:0] addr = 0;
+	always @(posedge CLOCK_50)
+	begin
+		addr <= addr + 1;
+	end
 	
-	wire [3:0] test;
-	assign test = {1'b0, command_was_sent, error_communication_timed_out, received_data_en};
-	
-	hex H5(.in(received_data[7:4]), .out(HEX5));
-	hex H4(.in(received_data[3:0]), .out(HEX4));
-	hex H0(.in(test), .out(HEX0));
-	
-	assign LEDR0 = received_data[0];
-	
-	//PS2 inst (
-	//	.clk_clk(CLOCK_50),
-	//	.ps2_0_external_interface_CLK(PS2_CLK),
-	//	.ps2_0_external_interface_DAT(PS2_DATA),
-	//	.reset_reset_n(KEY0),
-	//	.readdata(readdata)
-	//);
-	
-	//wire [15:0] data;
-	//assign data = PS2_DATA;
-	
-	//hex H5(.in(readdata[31:28]), .out(HEX5));
-	//hex H4(.in(readdata[27:24]), .out(HEX4));
-	//hex H3(.in(readdata[23:20]), .out(HEX3));
-	//hex H2(.in(readdata[19:16]), .out(HEX2));
-	//hex H1(.in(readdata[15:12]), .out(HEX1));
-	//hex H0(.in(readdata[11:8]), .out(HEX0));
-	
-	//assign LEDR0 = TEST;
+	altsyncram	altsyncram_component (
+				  .address_a (addr),
+				  .clock0 (CLOCK_50),
+				  .data_a (received_data),
+				  .wren_a (received_data_en),
+				  .q_a (memData),
+				  .aclr0 (1'b0),
+				  .aclr1 (1'b0),
+				  .addressstall_a (1'b0),
+				  .byteena_a (1'b1),
+				  .clocken0 (1'b1),
+				  .clocken1 (1'b1),
+				  .clocken2 (1'b1),
+				  .clocken3 (1'b1),
+				  .eccstatus (),
+				  .rden_a (1'b0)
+				  );
+defparam
+		altsyncram_component.clock_enable_input_a = "BYPASS",
+		altsyncram_component.clock_enable_output_a = "BYPASS",
+		altsyncram_component.intended_device_family = "Cyclone V",
+		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=YES, INSTANCE_NAME=DATA_MEM",
+		altsyncram_component.lpm_type = "altsyncram",
+		altsyncram_component.numwords_a = 65536,
+		altsyncram_component.operation_mode = "SINGLE_PORT",
+		altsyncram_component.outdata_aclr_a = "NONE",
+		altsyncram_component.outdata_reg_a = "UNREGISTERED",
+		altsyncram_component.power_up_uninitialized = "FALSE",
+		altsyncram_component.widthad_a = 16,
+		altsyncram_component.width_a = 8,
+		altsyncram_component.width_byteena_a = 1;
+
 	
 endmodule
